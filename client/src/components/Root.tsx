9d9d606bde2a3e7fe2661a7e44468fa6dbd6f005
Denis Makarov <limitofzero@gmail.com>
2018-03-13 23:15:57 +0300

    Additional settings. Removed Chat.tsx. Added new npm modules.

--- client/src/components/Root.tsx
+++ client/src/components/Root.tsx
@@ -1,17 +1,9 @@
 import * as React from 'react';
-import Chat from './Chat';
 import "../styles/components/c-root.scss";
 
-interface RootProps {}
-
-export default class Root extends React.Component<RootProps> {
-    constructor(props: RootProps) {
-        super(props);
-    }
-
+export default class Root extends React.Component<any> {
     render() {
         return <div className="container">
-            <div className="row"><Chat/></div>
         </div>;
     }
 }
