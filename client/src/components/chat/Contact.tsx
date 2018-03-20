e9d111a129acf57b31aebe687d6f991c1a761308
Denis Makarov <limitofzero@gmail.com>
2018-03-20 23:08:06 +0300

    Rewrite Contact component

--- client/src/components/chat/Contact.tsx
+++ client/src/components/chat/Contact.tsx
@@ -1,15 +1,16 @@
 import * as React from 'react';
-import List from 'material-ui/List';
 import Props from '../../interfaces/Contact';
-import {ListItemIcon, ListItemText} from 'material-ui/List';
+import { ListItem, ListItemIcon, ListItemText } from "material-ui/List";
+import Avatar from 'material-ui/Avatar';
 
 export default class Contact extends React.Component<Props> {
     
     render() {
-        const {name} = this.props;
+        const {name, avatarUrl} = this.props;
 
-        return <div className='contact'>
-            <div className='contact__name'>{name}</div>
-        </div>
+        return <ListItem button>
+            <Avatar alt="Remy Sharp" src={avatarUrl} />
+            <ListItemText primary={name} />
+        </ListItem>
     }
 }
--- client/src/components/chat/ContactList.tsx
+++ client/src/components/chat/ContactList.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import List, { ListItem } from "material-ui/List";
+import List from "material-ui/List";
 import Props from '../../interfaces/ContactList';
 import Contact from './Contact';
 import '../../styles/components/contact-list.scss';
@@ -11,7 +11,7 @@ export default class ContactList extends React.Component<Props> {
 
         return <div className='contact-list'>
             <List>
-                {list.map(contact=> <ListItem button key={contact.id}><Contact {...contact}/></ListItem>)}
+                {list.map(contact => <Contact key={contact.id} {...contact}/>)}
             </List>
         </div>
     }
