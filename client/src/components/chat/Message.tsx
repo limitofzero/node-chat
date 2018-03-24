084cf70eb912429128719166fb07b6b8ab8b5a43
Denis Makarov <limitofzero@gmail.com>
2018-03-24 11:36:52 +0300

    Changed some styles for components

--- client/src/components/chat/Message.tsx
+++ client/src/components/chat/Message.tsx
@@ -1,17 +1,15 @@
 import * as React from 'react';
 import Props from '../../interfaces/Message';
-import { ListItem, ListItemSecondaryAction, ListItemText } from 'material-ui/List';
-import Paper from 'material-ui/Paper';
+import '../../styles/components/message.scss';
+
 
 export default class Message extends React.Component<Props> {
     render() {
         const {author, text} = this.props;
         
-        return <Paper className="message">
-            <ListItem>
+        return <div className="message">
                 <div className="message__author">{author}</div>
                 <div className="message_text">{text}</div>
-            </ListItem>
-        </Paper>;
+            </div>;
     }
 }
--- client/src/components/chat/MessageWindow.tsx
+++ client/src/components/chat/MessageWindow.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import Props from '../../interfaces/MessageWindow';
 import Message from './Message';
 import Grid from 'material-ui/Grid';
-import List from 'material-ui/List';
 import Paper from 'material-ui/Paper';
 import '../../styles/components/message-window.scss';
 
@@ -11,11 +10,10 @@ export default class MessageWindow extends React.Component<Props> {
         const {list} = this.props;
 
         return <div className="message-window">
-                    <List className="message-window__list">
+                    <ul className="message-window__list">
                         {list.map(ms => <Paper className="message-window__message">
-                            <Message key={ms.id} {...ms}/>
-                        </Paper>)}
-                    </List>
+                            <Message key={ms.id} {...ms}/></Paper>)}
+                    </ul>
                 </div>;
     }
 }
