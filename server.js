cc8f758eaec795411debd9020d6a05092d783a7f
Denis Makarov <limitofzero@gmail.com>
2018-03-25 21:14:05 +0300

    Listening connected event

--- server.js
+++ server.js
@@ -9,6 +9,13 @@ const port = 8080;
 server.listen(port);
 
 logger.debug('Servar has been started...');
-
 app.use(express.static(__dirname + '/dist'));
+
+io.on('connection', (socket) => {
+    const name = 'U' + (socket.id).toString().substr(1, 4);
+    socket.broadcast.emit('newUser', name);
+    socket.emit('userName', name);
+    
+    logger.info(name + ' connected to chat');
+})
 
