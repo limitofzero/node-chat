c0bfc624c62085c9e13b765a8e2b41901df49148
Denis Makarov <limitofzero@gmail.com>
2018-03-25 21:02:48 +0300

    Rewrite server js

--- server.js
+++ server.js
@@ -1,46 +1,14 @@
-const http = require('http'),
-    fs = require('fs'),
-    path = require('path'),
-    mime = require('mime');
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const log4js = require('log4js');
+const logger = log4js.getLogger();
 
-const cache = {};
+const port = 8080;
+server.listen(port);
 
-function send404(resp) {
-    resp.writeHead(404, {'Content-Type': 'text/plain'});
-    resp.write('Error 404: resource not found!');
-    resp.end();
-}
+logger.debug('Servar has been started...');
 
-function sendFile(resp, filePath, fileContents) {
-    resp.writeHead(200, {"content-type": mime.lookup(path.basename(filePath))});
-    resp.end(fileContents);
-}
+app.use(express.static(__dirname + '/dist'));
 
-function serveStatic(resp, cache, absPath) {
-    if (cache[absPath]) {
-        sendFile(resp, absPath, cache[absPath]);
-    } else {
-        fs.exists(absPath,exists => {
-            if (exists) {
-                fs.readFile(absPath,(err, data) => {
-                    if(err) {
-                        send404(resp);
-                    } else {
-                        cache[absPath] = data;
-                        sendFile(resp, absPath, data);
-                    }
-                });
-            } else {
-                send404(resp);
-            }
-        })
-    }
-}
-
-const server = http.createServer((request, response)=> {
-    const filePath = request.url === '/' ? 'public/index.html' : 'public' + request.url;
-    const absPath = './' + filePath;
-    serveStatic(response, cache, absPath);
-});
-
-server.listen(3000, () => console.log("Server listening on port 3000."));
