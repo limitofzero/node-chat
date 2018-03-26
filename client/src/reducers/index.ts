a3194d211e6dc3ab0eb5165ef1b5c76a8a3b1be5
Denis Makarov <limitofzero@gmail.com>
2018-03-26 22:07:21 +0300

    Added combine reducer

---
+++ client/src/reducers/index.ts
@@ -0,0 +1,8 @@
+import {combineReducers} from 'redux';
+import contactReducer from '../reducers/contactReducer';
+
+const chatReducers = combineReducers({
+    contactReducer
+});
+
+export default chatReducers;
