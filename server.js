d79a31529cd8e45fe4f2efeaa5dbdcaa4cc7d116
Макаров Денис <limitofzero@gmail.com>
2018-04-01 14:55:25 +0300

    Added disconnect event into server.js

--- server.js
+++ server.js
@@ -13,24 +13,35 @@ server.listen(port);
 logger.debug('Server has been started...');
 app.use(express.static(__dirname + '/dist'));
 
-const contacts = [];
+let contacts = [];
 
-io.on('connection', (socket) => {
-    const name = 'U' + (socket.id).toString().substr(1, 4);
+const getNewContact = (sockId) => {
+    const name = 'U' + (sockId).toString().substr(1, 4);
 
     const contact = {
-        id: socket.id, 
+        id: sockId, 
         avatarUrl: '', 
-        name: name
-    }
+        name
+    };
 
+    return contact;
+} 
+
+io.on('connection', (socket) => {
+    const contact = getNewContact(socket.id);
     contacts.push(contact);
+    logger.info(contact.name + ' connected to chat');
 
     socket.broadcast.emit('ADD_CONTACT', contact);
     socket.emit('SOCKET_CONNECTED', contact);
     socket.emit('RECEIVE_CONTACT_LIST', contacts);
     
-    logger.info(name + ' connected to chat');
+    socket.on('disconnect', (reason) => {
+        contacts = contacts.filter(contact => contact.id !== socket.id);
+        socket.broadcast.emit('DELETE_CONTACT', {id: socket.id});
+
+        logger.info(socket.id + ' disconnected from chat');
+    });
 });
 
 
