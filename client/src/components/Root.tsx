20b16185f3a6c5c9171e57e85ad1e8421b2c55a1
Макаров Денис <limitofzero@gmail.com>
2018-04-10 23:31:28 +0300

    Added sign form prototype

--- client/src/components/Root.tsx
+++ client/src/components/Root.tsx
@@ -6,12 +6,14 @@ import Grid from 'material-ui/Grid';
 import "../styles/common.scss";
 import "../styles/components/root.scss";
 import { Route } from 'react-router';
+import SignInForm from './signInForm/SignInForm';
 
 export default class Root extends React.Component<any> {
     render() {
         return <Provider store={store}>
                 <div className="root">
                     <Route exact path='/' component={Chat}/>
+                    <Route path='/login' component={SignInForm}/>
                 </div>
             </Provider>;
     }
