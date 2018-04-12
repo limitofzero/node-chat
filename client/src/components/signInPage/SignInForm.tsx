77bda9671b765e03e8d2a896e6df678efcb1571b
Макаров Денис <limitofzero@gmail.com>
2018-04-12 23:18:11 +0300

    Input and checkbox handlers

--- client/src/components/signInPage/SignInForm.tsx
+++ client/src/components/signInPage/SignInForm.tsx
@@ -2,13 +2,28 @@ import * as React from 'react';
 import { FormControlLabel, Button, Checkbox, TextField } from 'material-ui';
 
 export default class SignInForm extends React.Component<{}> {
+    state = {
+        login: '',
+        password: '',
+        rememberMe: false
+    }
+
+    handleInputChange = (event: React.FormEvent<HTMLInputElement>, field: string) : void => {
+        this.setState({[field]: event.currentTarget.value});
+    }
+
+    handleCheckboxChange = (event: React.FormEvent<HTMLInputElement>) : void => {
+        this.setState({rememberMe: event.currentTarget.checked});
+    }
 
     render() {
         return <form className='sign-in_form'>
             <TextField id='login'
                 label='Login'
                 margin='normal'
+                autoFocus={true}
                 className='sing-in_field'
+                onChange={(ev) => this.handleInputChange(ev, 'login')}
             />
             <TextField
                 id='password'
@@ -17,12 +32,12 @@ export default class SignInForm extends React.Component<{}> {
                 type='password'
                 autoComplete='current-password'
                 margin='normal'
+                onChange={(ev) => this.handleInputChange(ev, 'password')}
             />
             <FormControlLabel
                 control={
                     <Checkbox
-                    checked={false}
-                    value='false'
+                    onChange={(ev) => this.handleInputChange(ev, 'rememberMe')}
                     />
                 }
                 label='Remember me'
