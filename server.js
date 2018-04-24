2428c8db617ebb6d58e4bc7aee2824bd385edd92
Макаров Денис <limitofzero@gmail.com>
2018-04-24 23:14:10 +0300

    Configured routing

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const server = require('http').Server(app);
 const io = require('socket.io')(server);
 const log4js = require('log4js');
 const logger = log4js.getLogger();
+const path = require('path');
 
 logger.level = 'debug';
 
@@ -11,7 +12,12 @@ const port = 8080;
 server.listen(port);
 
 logger.debug('Server has been started...');
+
 app.use(express.static(__dirname + '/dist'));
+
+app.get('*', function (request, response){
+    response.sendFile(path.resolve(__dirname, 'dist', 'index.html'))
+});
 
 let contacts = [],
     messages = [];
@@ -58,10 +64,6 @@ io.on('connection', (socket) => {
         socket.broadcast.emit('RECEIVE_MESSAGE', message);
     })
 });
-
-io.on('SEND_MESSAGE', (socket) => {
-    console.log('in on');
-})
 
 
 
