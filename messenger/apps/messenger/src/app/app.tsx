d19083afabc14e85ef20d9968d471cf3dfba9686
Denis Makarov <denis.makarov@piano.io>
2020-06-27 12:59:21 +0300

    (feature): add material ui

--- messenger/apps/messenger/src/app/app.tsx
+++ messenger/apps/messenger/src/app/app.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Message } from '@messenger/api-interfaces';
+import { Grid, AppBar, Toolbar, Typography } from "@material-ui/core";
 
 export const App = () => {
   const [m, setMessage] = useState<Message>({ message: '' });
@@ -11,16 +12,22 @@ export const App = () => {
   }, []);
 
   return (
-    <>
-      <div style={{ textAlign: 'center' }}>
-        <h1>Welcome to messenger!</h1>
-        <img
-          width="450"
-          src="https://raw.githubusercontent.com/nrwl/nx/master/images/nx-logo.png"
-        />
-      </div>
-      <div>{m.message}</div>
-    </>
+    <Grid>
+      <AppBar position='static'>
+        <Toolbar>
+          <Typography variant="h6">
+            React messenger
+          </Typography>
+        </Toolbar>
+      </AppBar>
+        <div style={{ textAlign: 'center' }}>
+          <img
+            width="450"
+            src="https://raw.githubusercontent.com/nrwl/nx/master/images/nx-logo.png"
+          />
+        </div>
+        <div>{m.message}</div>
+    </Grid>
   );
 };
 
