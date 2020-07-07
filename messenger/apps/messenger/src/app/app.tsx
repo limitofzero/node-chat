f1fa0a49cab35b8664b3913d92f88cee2ef18827
Denis Makarov <denis.makarov@piano.io>
2020-07-07 22:41:14 +0300

    feat(front) add router

--- messenger/apps/messenger/src/app/app.tsx
+++ messenger/apps/messenger/src/app/app.tsx
@@ -1,6 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { Message } from '@messenger/api-interfaces';
 import { Grid, AppBar, Toolbar, Typography } from "@material-ui/core";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 export const App = () => {
   const [m, setMessage] = useState<Message>({ message: '' });
@@ -12,22 +17,31 @@ export const App = () => {
   }, []);
 
   return (
-    <Grid>
-      <AppBar position='static'>
-        <Toolbar>
-          <Typography variant="h6">
-            React messenger
-          </Typography>
-        </Toolbar>
-      </AppBar>
-        <div style={{ textAlign: 'center' }}>
-          <img
-            width="450"
-            src="https://raw.githubusercontent.com/nrwl/nx/master/images/nx-logo.png"
-          />
-        </div>
-        <div>{m.message}</div>
-    </Grid>
+    <Router>
+      <Switch >
+        <Route exact path="/">
+          <Grid>
+            <AppBar position='static'>
+              <Toolbar>
+                <Typography variant="h6">
+                  React messenger
+                </Typography>
+              </Toolbar>
+            </AppBar>
+            <div style={{ textAlign: 'center' }}>
+              <img
+                width="450"
+                src="https://raw.githubusercontent.com/nrwl/nx/master/images/nx-logo.png"
+              />
+            </div>
+            <div>{m.message}</div>
+          </Grid>
+        </Route>
+        <Route path="/auth">
+          <h1>Test</h1>
+        </Route>
+      </Switch>
+    </Router>
   );
 };
 
