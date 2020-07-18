9edeacfeceae48e2cb4dcb2cf692263e20de90a4
limitofzero <limitofzero@gmail.com>
2020-07-18 23:42:31 +0300

    (feature): add tab value

--- messenger/apps/messenger/src/app/auth/auth-form.tsx
+++ messenger/apps/messenger/src/app/auth/auth-form.tsx
@@ -1,14 +1,22 @@
-import { Grid, Paper, AppBar, Tabs, Tab } from '@material-ui/core';
-import React from 'react';
+import { Grid, AppBar, Tabs, Tab } from '@material-ui/core';
+import React, { useState } from 'react';
+
+type AuthForms = 'sign-in' | 'sign-up';
 
 export const AuthForm = () => {
+  const [form, setForm] = useState<AuthForms>('sign-in');
+
+  const handleTabChange = (event, newValue) => {
+    setForm(newValue);
+  };
+
   return (
     <Grid container justify='center'>
       <Grid item>
         <AppBar position='static'>
-          <Tabs>
-            <Tab label='Sign In'></Tab>
-            <Tab label='Sign up'></Tab>
+          <Tabs value={form} onChange={handleTabChange}>
+            <Tab label='Sign In' value='sign-in'/>
+            <Tab label='Sign up' value='sign-up'/>
           </Tabs>
         </AppBar>
       </Grid>
