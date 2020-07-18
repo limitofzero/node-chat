c399f6a4c9e13c6f324a03fcdf7f37470201ff5a
limitofzero <limitofzero@gmail.com>
2020-07-18 23:49:26 +0300

    (feature): add tab visibility

--- messenger/apps/messenger/src/app/auth/auth-form.tsx
+++ messenger/apps/messenger/src/app/auth/auth-form.tsx
@@ -18,6 +18,8 @@ export const AuthForm = () => {
             <Tab label='Sign In' value='sign-in'/>
             <Tab label='Sign up' value='sign-up'/>
           </Tabs>
+          <div hidden={form === 'sign-in'}> This will be sign in form!</div>
+          <div hidden={form === 'sign-up'}> This will be sign up form!</div>
         </AppBar>
       </Grid>
     </Grid>
