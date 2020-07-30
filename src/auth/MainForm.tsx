f88764d9ad4d7b3355c6ed66b180da7fee9300bb
limitofzero <limitofzero@gmail.com>
2020-07-30 22:23:51 +0300

    feat(client) add working tabs

--- src/auth/MainForm.tsx
+++ src/auth/MainForm.tsx
@@ -2,18 +2,25 @@ import React from 'react';
 import {Tab, Tabs} from "@blueprintjs/core";
 import './MainForm.css';
 
+type AuthForm = 'sign-in' | 'sign-up';
+
 export const MainForm = () => {
+    const [currentForm, setForm] = React.useState<AuthForm>('sign-in');
+
+    const handleTabChange = (newTabId: AuthForm) => {
+        setForm(newTabId);
+    };
+
     return (
         <div className="row">
             <div className="col-xs-4 col-xs-offset-4">
                 <div className='main-form'>
-                    <Tabs id="TabsExample" selectedTabId="rx">
-                        <Tab id="ng" title="Angular"/>
-                        <Tab id="mb" title="Ember" panelClassName="ember-panel"/>
-                        <Tab id="rx" title="React"/>
+                    <Tabs id="TabsExample" onChange={handleTabChange} selectedTabId={currentForm}>
+                        <Tab id='sign-in' title="Sign in"/>
+                        <Tab id="sign-up" title="Sign up"/>
                     </Tabs>
                 </div>
             </div>
         </div>
-    )
+    );
 };
