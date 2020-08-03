f3ac1a55786c77ff60015920ce3f76dcac8db249
limitofzero <limitofzero@gmail.com>
2020-08-03 13:20:19 +0300

    feat(client) envelop icon

--- src/auth/sign-up/SignUp.tsx
+++ src/auth/sign-up/SignUp.tsx
@@ -4,8 +4,8 @@ export const SignUp = () => {
     return (
         <div>
             <div className="bp3-input-group">
-                <input type="text" className="bp3-input" placeholder="Filter histogram..."/>
-                <span className="bp3-icon bp3-icon-filter"/>
+                <input type="text" className="bp3-input" placeholder="Email..."/>
+                <span className="bp3-icon bp3-icon-envelope"/>
             </div>
         </div>
     );
