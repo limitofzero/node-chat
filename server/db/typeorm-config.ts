1931f683888f20d432b5fa9f63b599385a329eb3
limitofzero <limitofzero@gmail.com>
2020-08-04 21:41:42 +0300

    feat(server): add postgress connection

---
+++ server/db/typeorm-config.ts
@@ -0,0 +1,20 @@
+import {PostgresConnectionOptions} from "typeorm/driver/postgres/PostgresConnectionOptions";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const {env} = process;
+
+export const typeOrmConfig: PostgresConnectionOptions = {
+    type: 'postgres',
+    host: env.POSTGRES_HOST,
+    port: parseInt(env.POSTGRES_PORT as string),
+    username: env.POSTGRES_USER,
+    password: env.POSTGRES_PASSWORD,
+    database: env.POSTGRES_DATABASE,
+    synchronize: true,
+    logging: false,
+    entities: [
+        './db/**/*.ts'
+    ]
+};
--- server/main.ts
+++ server/main.ts
@@ -1,15 +1,16 @@
 import express from 'express';
-import dotenv from 'dotenv';
+import "reflect-metadata";
+import {createConnection} from "typeorm";
+import {typeOrmConfig} from "./db/typeorm-config";
 
-dotenv.config();
-const app = express();
+createConnection(typeOrmConfig).then(async connection => {
+    const app = express();
 
-const port = process.env.PORT;
+    const port = process.env.PORT;
 
-app.get('/', (req, res) => {
-    res.send("Hi, man!")
-});
+    app.get('/', (req, res) => {
+        res.send("Hi, man!")
+    });
 
-app.listen(port, () => console.log(`Server started on ${port}`));
-
-
+    app.listen(port, () => console.log(`Server started on ${port}`));
+}).catch(err => console.log(err));
