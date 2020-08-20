77658e1d32590224693dd6bc2658f0a163c02e23
limitofzero <limitofzero@gmail.com>
2020-08-20 18:40:58 +0300

    feature: add expiresIn for jwt

--- server/src/controllers/auth/post-login.ts
+++ server/src/controllers/auth/post-login.ts
@@ -17,11 +17,11 @@ export const login = async (request: Request, response: Response): Promise<Respo
         return response.status(400).json({ errors: errors.array() });
     }
 
-    const { email, password } = request.body as SignInForm;
+    const { email, password, rememberMe } = request.body as SignInForm;
     const userRep = getRepository(User);
 
     return userRep.findOneOrFail({ where: { email } })
-      .then(user => user.isPasswordValid(password) ? returnToken(user) : throwError())
+      .then(user => user.isPasswordValid(password) ? returnToken(user, rememberMe) : throwError())
       .then(result => response.json(result))
       .catch(err => response.json({ error: err.message }));
 };
@@ -30,13 +30,18 @@ const throwError = () => {
     throw new Error("Email or password is not valid");
 };
 
-const returnToken = (user: User): SuccessLogin => {
+const returnToken = (user: User, rememberMe: boolean): SuccessLogin => {
     const { username, email } = user;
+    const expiresIn = getExpiresIn(rememberMe);
 
     const token = jwt.sign({
         username,
         email
-    }, "test");
+    }, "test", { expiresIn });
 
     return { token };
+};
+
+const getExpiresIn = (rememberMe: boolean): string => {
+    return rememberMe ? "30d" : "1h";
 };
