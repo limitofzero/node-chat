f7844c8d88ce58e447589f4bebb3303afd4fc9c4
limitofzero <limitofzero@gmail.com>
2020-10-11 21:40:19 +0300

    feat(): add nestjs

--- server/main.ts
+++ server/main.ts
@@ -1,21 +1,8 @@
-import express from "express";
-import "reflect-metadata";
-import { createConnection } from "typeorm";
-import { typeOrmConfig } from "./src/db/typeorm-config";
-import bodyParser from "body-parser";
-import cors from "cors";
-import { auth } from "./src/controllers/auth";
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 
-createConnection(typeOrmConfig).then(async connection => {
-    const app = express();
-    app.use(bodyParser.json());
-
-    if (process.env.ENV === "DEV") {
-        app.use(cors());
-    }
-
-    const port = process.env.PORT;
-    app.use(auth);
-
-    app.listen(port, () => console.log(`Server started on ${port}`));
-}).catch(err => console.log(err));
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
+}
+bootstrap();
