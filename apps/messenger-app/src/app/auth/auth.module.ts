346066db20a2c0aa1d1cbf82e393ac8e1435274f
limitofzero <limitofzero@gmail.com>
2020-10-31 22:23:10 +0300

    feat(): add form

--- apps/messenger-app/src/app/auth/auth.module.ts
+++ apps/messenger-app/src/app/auth/auth.module.ts
@@ -5,6 +5,11 @@ import { AuthRoutingModule } from "./auth-routing.module";
 import { LoginComponent } from "./login/login.component";
 import { MatCardModule } from "@angular/material/card";
 import { MatTabsModule } from "@angular/material/tabs";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatIconModule } from "@angular/material/icon";
+import { MatInputModule } from "@angular/material/input";
+import { MatCheckboxModule } from "@angular/material/checkbox";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [AuthFormComponent, LoginComponent],
@@ -12,7 +17,13 @@ import { MatTabsModule } from "@angular/material/tabs";
     CommonModule,
     AuthRoutingModule,
     MatCardModule,
-    MatTabsModule
+    MatTabsModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatCheckboxModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class AuthModule {
--- apps/messenger-app/src/app/auth/login/login.component.ts
+++ apps/messenger-app/src/app/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
   selector: "messenger-login",
@@ -6,8 +7,16 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./login.component.scss"]
 })
 export class LoginComponent implements OnInit {
+  public form: FormGroup;
 
-  constructor() {
+  constructor(
+    private readonly fb: FormBuilder
+  ) {
+    this.form = fb.group({
+      email: fb.control("", [Validators.required, Validators.email]),
+      password: fb.control("", [Validators.required]),
+      rememberMe: fb.control(true)
+    });
   }
 
   ngOnInit(): void {
