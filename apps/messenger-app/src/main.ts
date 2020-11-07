8ebdde17e375dc51083d04248dfa2d6ddab7f3b5
limitofzero <limitofzero@gmail.com>
2020-11-07 11:38:30 +0300

    feat(): add Akita prod mode

--- apps/messenger-app/src/main.ts
+++ apps/messenger-app/src/main.ts
@@ -1,11 +1,16 @@
 import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+import { AppModule } from "./app/app.module";
+import { environment } from "./environments/environment";
+import { enableAkitaProdMode } from "@datorama/akita";
 
 if (environment.production) {
   enableProdMode();
+}
+
+if (environment.production) {
+  enableAkitaProdMode();
 }
 
 platformBrowserDynamic()
