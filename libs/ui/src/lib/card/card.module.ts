d17393e027d19a34418f2e2fdd5982a80b02f76b
limitofzero <limitofzero@gmail.com>
2021-01-30 23:30:32 +0300

    feat(front): add card-title-text component

--- libs/ui/src/lib/card/card.module.ts
+++ libs/ui/src/lib/card/card.module.ts
@@ -3,16 +3,18 @@ import { CommonModule } from "@angular/common";
 import { CardComponent } from "./card/card.component";
 import { CardContentComponent } from "./card-content/card-content.component";
 import { CardTitleComponent } from "./card-title/card-title.component";
+import { CardTitleTextComponent } from "./card-title-text/card-title-text.component";
 
 @NgModule({
-  declarations: [CardComponent, CardContentComponent, CardTitleComponent],
+  declarations: [CardComponent, CardContentComponent, CardTitleComponent, CardTitleTextComponent],
   imports: [
     CommonModule
   ],
   exports: [
     CardComponent,
     CardContentComponent,
-    CardTitleComponent
+    CardTitleComponent,
+    CardTitleTextComponent
   ]
 })
 export class CardModule {
