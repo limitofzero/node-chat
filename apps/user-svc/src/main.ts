a6325419d0c2f6654812a55ee5a0418dd9926ac5
limitofzero <limitofzero@gmail.com>
2021-02-10 18:33:58 +0300

    feat(): add user svc

---
+++ apps/user-svc/src/main.ts
@@ -0,0 +1,31 @@
+/**
+ * This is not a production server yet!
+ * This is only a minimal backend to get started.
+ */
+
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+
+import { AppModule } from './app/app.module';
+import { Transport } from '@nestjs/microservices';
+import { join } from 'path';
+
+async function bootstrap() {
+  const app = await NestFactory.createMicroservice(AppModule, {
+    transport: Transport.GRPC,
+    options: {
+      package: 'user',
+      protoPath: join(process.cwd(), 'proto/microservices/user/user.proto'),
+    }
+  });
+
+  app.useLogger(Logger);
+  app.useGlobalPipes(new ValidationPipe());
+
+  await app.listenAsync();
+}
+
+bootstrap().catch(err => {
+  Logger.error(err);
+  process.exit(1);
+});
